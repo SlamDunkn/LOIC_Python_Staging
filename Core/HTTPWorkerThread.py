94de9383b11e359d3f991c4cd317f998df56f84a
YmxldnB4 <M8R-1vnayb@mailinator.com>
2010-12-30 17:40:26 +0100

    Add more useragents to choose from, use random.choice to pick one, and make it a global

--- Core/HTTPWorkerThread.py
+++ Core/HTTPWorkerThread.py
@@ -2,6 +2,29 @@ import socks, time
 from Functions import *
 from multiprocessing import Process
 
+useragents = [
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)",
+    "Googlebot/2.1 (http://www.googlebot.com/bot.html)", # rly?
+    "Opera/9.20 (Windows NT 6.0; U; en)",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.1) Gecko/20061205 Iceweasel/2.0.0.1 (Debian-2.0.0.1+dfsg-2)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)",
+    "Opera/10.00 (X11; Linux i686; U; en) Presto/2.2.0",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; he-IL) AppleWebKit/528.16 (KHTML, like Gecko) Version/4.0 Safari/528.16",
+    "Mozilla/5.0 (compatible; Yahoo! Slurp/3.0; http://help.yahoo.com/help/us/ysearch/slurp)", # maybe not
+    "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101209 Firefox/3.6.13"
+    "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 5.1; Trident/5.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)",
+    "Mozilla/4.0 (compatible; MSIE 6.0b; Windows 98)",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)",
+    "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.8) Gecko/20100804 Gentoo Firefox/3.6.8",
+    "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.7) Gecko/20100809 Fedora/3.6.7-1.fc14 Firefox/3.6.7",
+    "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)", # meh
+    "Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)", # also meh
+    "YahooSeeker/1.2 (compatible; Mozilla 4.0; MSIE 5.5; yahooseeker at yahoo-inc dot com ; http://help.yahoo.com/help/us/shop/merchant/)" # more meh
+]
+
 class HTTPWorkerThread(Process):
 
     def __init__(self, flooder, id):
@@ -14,18 +37,6 @@ class HTTPWorkerThread(Process):
         self.socks5ip = flooder.socks5ip
         self.socks5port = flooder.socks5port
         self.running = True
-
-        self.useragents = {1 : 'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 5.1; Trident/5.0)',
-                2 : 'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-                3 : 'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)',
-                4 : 'Mozilla/4.0 (compatible; MSIE 6.0b; Windows 98)',
-                5 : 'Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)',
-                6 : 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.8) Gecko/20100804 Gentoo Firefox/3.6.8',
-                7 : 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.7) Gecko/20100809 Fedora/3.6.7-1.fc14 Firefox/3.6.7',
-                8 : 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-                9 : 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)',
-                10 : 'Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)',
-                11 : 'YahooSeeker/1.2 (compatible; Mozilla 4.0; MSIE 5.5; yahooseeker at yahoo-inc dot com ; http://help.yahoo.com/help/us/shop/merchant/)' }
 
         if flooder.random:
             self.message = randomString(256)
@@ -45,7 +56,7 @@ class HTTPWorkerThread(Process):
                           "Keep-Alive: 900\r\n"
                           "Content-Length: 10000\r\n"
                           "Content-Type: application/x-www-form-urlencoded\r\n\r\n" % 
-                          (self.host, self.useragents[random.randrange(1,12)]))
+                          (self.host, random.choice(useragents)))
         #print "thread", self.id, "send post"
         
         for i in range(0, 9999):
